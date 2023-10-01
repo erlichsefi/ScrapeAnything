31f9d08bb4463ccfc935c78279d6d0da022bc48a
Erlich Sefi <erlichsefi@gmail.com>
2023-10-01 17:34:53 +0000

    remove old files

--- project_extension/chrome_extension/options.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("optionsForm").addEventListener("submit", function(e) {
-    e.preventDefault();
-    let apiKey = document.getElementById("apiKey").value;
-    chrome.storage.sync.set({apiKey: apiKey}, function() {
-        console.log("API key saved");
-    });
-});
--- project_extension/chrome_extension/scripts/window.js
+++
@@ -1,9 +0,0 @@
-function windowFunction() {
-const viewpointscroll = window.pageYOffset || document.documentElement.scrollTop;
-const viewportHeight = window.innerheight || document.documentElement.clientHeight;
-// Create a formatted string containing the values
-const formattedValues = `viewpointscroll:${viewpointscroll}, viewportHeight:${viewportHeight}`;
-
-// Return the formatted string
-return formattedValues;
-}
