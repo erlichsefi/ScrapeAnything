33b8ed131cf77fe8b80302335b021c03acd96f13
Erlich Sefi <erlichsefi@gmail.com>
2023-10-17 06:55:16 +0000

    .

--- extension/shared/extract/window.js
+++ extension/shared/extract/window.js
@@ -2,7 +2,7 @@ export default function get_viewpoint(){
     const viewpointscroll = window.pageYOffset || document.documentElement.scrollTop;
     const viewportHeight = window.innerheight || document.documentElement.clientHeight;
 
-    console.log("viewportHeight="+viewportHeight+"viewportHeight="+viewportHeight)
+    //console.log("viewportHeight="+viewportHeight+"viewportHeight="+viewportHeight)
     return {"viewpointscroll":viewpointscroll,"viewportHeight":viewportHeight}
 }
 // const {viewpointscroll,viewportHeight} = get_viewpoint()
